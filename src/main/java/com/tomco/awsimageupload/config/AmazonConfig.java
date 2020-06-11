420bf7851a5805c9e72361547ffa105855a5f40e
tberey <tomberey1@gmail.com>
2020-06-11 21:59:54 +0100

    Initial Commit - See README.md, Entry 'Version 1.0.0'

--- src/main/java/com/tomco/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/tomco/awsimageupload/config/AmazonConfig.java
@@ -14,7 +14,7 @@ public class AmazonConfig {
 
     @Bean // Instantiate AmazonS3 class as a Bean. Provide correct credentials to build our S3 client, and access bucket.
     public AmazonS3 S3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials("AKIAJSU3OTGJCVSAECBQ","GXemB1BsoVY4U0If4cIl3+f9LL5oHu8G8JOnTqLg");
+        AWSCredentials awsCredentials = new BasicAWSCredentials("AKIAJSU3OTGJCVSAECB","GXemB1BsoVY4U0If4cIl3+f9LL5oHu8G8JOnTqL");
         return AmazonS3ClientBuilder.standard()
                 .withRegion("eu-west-2")
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
